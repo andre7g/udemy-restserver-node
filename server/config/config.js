dc4b7d90d8bf7f1f957e8909508aa0e966b2e9ad
Andre <andre7.guerra@gmail.com>
2021-02-08 13:20:41 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,17 @@
 //------------
 
 process.env.PORT = process.env.PORT || 3000;
+
+//-----------
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//-----------
+//Base de datos
+//-----------
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://andreg7:Wnw61u1Kvvnkz1Al@cluster0.8vpzz.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
