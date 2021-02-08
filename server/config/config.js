78dc8c326cd82bfa85924b1ba2874b0828cef7bd
Andre <andre7.guerra@gmail.com>
2021-02-08 14:28:41 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://andreg7:Wnw61u1Kvvnkz1Al@cluster0.8vpzz.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
